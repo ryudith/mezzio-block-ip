6fb24b995f861d932f115c71cb62a526632827d2
Ryudith <ryudith_anggriawan@yahoo.co.id>
2022-06-16 12:02:04 +0700

    remove unuse use library statement

--- src/Helper/BlockIPCli.php
+++ src/Helper/BlockIPCli.php
@@ -10,10 +10,8 @@ namespace Ryudith\MezzioBlockIp\Helper;
 use Exception;
 use Ryudith\MezzioBlockIp\Storage\FileSystemStorage;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
