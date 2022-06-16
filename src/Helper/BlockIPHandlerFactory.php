a760a09299e51adb9a91ba1f6d10f79cd7f957cb
Ryudith <ryudith_anggriawan@yahoo.co.id>
2022-06-16 13:33:57 +0700

    add class doc comment and default config values

--- src/Helper/BlockIPHandlerFactory.php
+++ src/Helper/BlockIPHandlerFactory.php
@@ -12,6 +12,12 @@ use Ryudith\MezzioBlockIp\Storage\FileSystemStorage;
 
 /**
  * Block IP web helper handler factory.
+ * 
+ * Configuration key required for web helper :
+ * 1. helper_url_param_op : URL query parameter name for operation, default 'op'.
+ * 2. helper_url_param_ip : URL query parameter name for IP value, default 'ip'.
+ * 3. blacklist_uri_path  : URL route for blacklist IP, default '/blockip/blacklist'.
+ * 4. whitelist_uri_path  : URL route for whitelist IP, default '/blockip/whitelist'.
  */
 class BlockIPHandlerFactory
 {
@@ -23,8 +29,15 @@ class BlockIPHandlerFactory
      */
     public function __invoke (ContainerInterface $container) : BlockIPHandler
     {
-        $config = $container->get('config');
+        $config = $container->get('config')['mezzio_block_ip'];
         $storage = $container->get(FileSystemStorage::class);
+
+        $config = array_merge_recursive([
+            'helper_url_param_op' => 'op',
+            'helper_url_param_ip' => 'ip',
+            'blacklist_uri_path' => '/blockip/blacklist',
+            'whitelist_uri_path' => '/blockip/whitelist',
+        ], $config);
 
         return new BlockIPHandler($config, $storage);
     }
